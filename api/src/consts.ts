b401a5498e1d96a6fa9141ba0ae22395d34742cc
qixing7373 <112518xq@gmail.com>
2026-04-27 23:47:01 +0800

    Update consts.ts

--- api/src/consts.ts
+++ api/src/consts.ts
@@ -3,11 +3,11 @@
  * @desc   集中管理保护期天数、批量大小、令牌有效期、角色层级等运行时常量
  */
 export const PROTECT_DAYS    = 90
-export const BATCH_SIZE      = 90
+export const BATCH_SIZE      = 1000
 export const TOKEN_LIFE      = 86400
-export const MAX_IMPORT      = 50000
+export const MAX_IMPORT      = 200000
 export const MAX_FILE_SIZE   = 20 * 1024 * 1024
-export const INSERT_BATCH_SIZE = 10
+export const INSERT_BATCH_SIZE = 100
 export const PHONE_REGEX     = /^\+[1-9]\d{6,14}$/
 export const SECONDS_PER_DAY = 86400
 
